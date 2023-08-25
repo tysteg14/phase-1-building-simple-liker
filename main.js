b8303cf7d4e68bac381ffe7a9256d3b4fd3e66af
Tyrell <tysteg14@gmail.com>
2023-08-25 17:02:11 -0500

    simple liker

--- main.js
+++ main.js
@@ -2,7 +2,50 @@
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
-// Your JavaScript code goes here!
+document.addEventListener("DOMContentLoaded", () => {
+  const errorModal = document.getElementById("modal");
+  const modalMessage = document.getElementById("modal-message");
+  const likeButton = document.querySelector(".like");
+  const likeGlyph = document.querySelector(".like-glyph");
+
+  // Simulate a server call
+  function mimicServerCall() {
+    return new Promise((resolve, reject) => {
+      // Simulate a response with a 50% chance of success
+      setTimeout(() => {
+        const isSuccess = Math.random() < 0.5;
+        if (isSuccess) {
+          resolve("Success");
+        } else {
+          reject("Server error occurred");
+        }
+      }, 1000);
+    });
+  }
+
+  likeButton.addEventListener("click", () => {
+    mimicServerCall()
+      .then(() => {
+        // Successful server response
+        likeGlyph.textContent = "♥";
+        likeGlyph.classList.add("activated-heart");
+      })
+      .catch((error) => {
+        // Server error response
+        modalMessage.textContent = error;
+        errorModal.classList.remove("hidden");
+        setTimeout(() => {
+          errorModal.classList.add("hidden");
+        }, 3000);
+      });
+  });
+
+  likeGlyph.addEventListener("click", () => {
+    likeGlyph.textContent = "♡";
+    likeGlyph.classList.remove("activated-heart");
+  });
+});
+
 
 
 
